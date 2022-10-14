cace2d826ceb6ad82890f77457e8f3857893102a
Likith1705 <114089035+Likith1705@users.noreply.github.com>
2022-10-14 11:03:34 +0530

    Update mail5.sh

--- mail5.sh
+++ mail5.sh
@@ -3,5 +3,6 @@ perc=`df -h . | awk -F " " 'NR==2{print $5}' | sed 's/%/ /g'`
 if [ $perc -ge 25 ];then
 
 
+
 	echo "memory reached maximum threshold value and disk usage $perc percentage" | mail -s "disk usage" likithbs703@gmail.com
 fi
